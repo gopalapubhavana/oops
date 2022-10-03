7ba081f8d24dff4aa526b54e7cd451c6667d7247
gopalapubhavana <101540110+gopalapubhavana@users.noreply.github.com>
2022-10-03 10:33:55 +0530

    Add files via upload

---
+++ oops2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float number1,number2,avg,sum;
+	cout << "enter two numbers";
+	cin >> number1;
+	cin >> number2;
+	sum = number1+number2;
+	avg = sum/2;
+	cout<<sum<<endl;
+	cout<<avg<<endl;
+	return 0;
+	}
+
